b5f17d4203a3fb6862bee9a2220fae1aee2f4c04
SATYENDRA SINGH <77849905+TheSatyendraSingh@users.noreply.github.com>
2021-02-03 11:28:04 +0530

    Add files via upload

---
+++ src/Sdata.jsx
@@ -0,0 +1,39 @@
+
+import css from "../src/images/css.jpg";
+import angular from "../src/images/angular.jpg";
+import express from "../src/images/express.jpg";
+import javascript2 from "../src/images/javascript2.jpg";
+import jquery from "../src/images/jquery.jpg";
+import mongodb from "../src/images/mongodb.jpeg";
+
+
+const Sdata = [
+  {
+    imgsrc: css,
+    title: " css web development",
+  },
+  {
+    imgsrc: javascript2,
+    title: " Javascript development",
+  },
+ 
+  {
+    imgsrc: express,
+    title: "Express development",
+  },
+  
+
+  {
+    imgsrc: angular,
+    title: "Angular development",
+  },
+  {
+    imgsrc: jquery,
+    title: "jquery development",
+  },
+  {
+    imgsrc: mongodb,
+    title: "mongodb development",
+  },
+]
+export default Sdata;
